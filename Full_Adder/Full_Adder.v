36b3dbcff25cec6538703508be7f22978670a626
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2024-05-18 16:08:45 -0500

    Add files via upload

---
+++ Full_Adder/Full_Adder.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+
+
+module Full_Adder(
+    input A,
+    input B,
+    input Cin,
+    output Sum,
+    output Cout
+    );
+ //data flow 
+ assign Sum= A^B^Cin;
+ assign Cout= (A&B)| (Cin&(A^B));
+endmodule
