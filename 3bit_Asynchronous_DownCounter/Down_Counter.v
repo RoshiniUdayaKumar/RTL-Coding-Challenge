47260e01145820a0d1818eac97bf976349fb47f0
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2024-06-22 08:06:46 -0500

    Add files via upload

---
+++ 3bit_Asynchronous_DownCounter/Down_Counter.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module Up_Counter(
+    input clk,reset,
+    output [2:0] Q,
+    output [2:0] Qbar
+    );
+	 
+T_ff ff0(1'b1,clk,reset,Q[0],Qbar[0]),
+    ff1(1'b1,Qbar[0],reset,Q[1],Qbar[1]),
+    ff2(1'b1,Qbar[1],reset,Q[2],Qbar[2]);
+
+endmodule
---
+++ 3bit_Asynchronous_DownCounter/Down_Counter_tb.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module Up_Counter_tb();
+	reg clk,reset;
+	wire [2:0] Q;
+	wire [2:0] Qbar;
+
+//module instantiation 
+Up_Counter dut(clk,reset,Q,Qbar);
+
+//generating clock
+	initial begin
+		clk = 1; reset = 1;
+		#10; clk=1; reset=0;
+	end
+	
+	always begin
+	forever #5 clk=~clk;
+	end
+      
+endmodule
