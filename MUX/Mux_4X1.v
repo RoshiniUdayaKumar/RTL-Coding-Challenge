d82861077468b1fd8f63396d83635166c33d7a68
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2025-03-04 02:58:16 -0600

    Update Mux_4X1.v

--- MUX/Mux_4X1.v
+++ MUX/Mux_4X1.v
@@ -8,10 +8,10 @@ output reg Y;
 
 always @(I0 or I1 or I2 or I3 or sel) begin
 case(sel)
-2'b00: Y<=I0;
-2'b01: Y<=I1;
-2'b10: Y<=I2;
-2'b11: Y<=I3;
+2'b00: Y=I0;
+2'b01: Y=I1;
+2'b10: Y=I2;
+2'b11: Y=I3;
 endcase
 end
 endmodule
