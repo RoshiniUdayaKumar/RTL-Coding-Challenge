7479ef2058ee52ae49cd4e4698ac1949d89e3e55
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2025-03-07 05:01:35 -0600

    Update Left_Shift.v

--- 4_bit_Shift_Register/Left_Shift_Register/Left_Shift.v
+++ 4_bit_Shift_Register/Left_Shift_Register/Left_Shift.v
@@ -1,18 +1,20 @@
-`timescale 1ns / 1ps
 
-module Left_Shift(
-    input clk,
-    input reset,
-    output [3:0] Q
-    );
- reg [3:0] tmp;
- 
-always @(posedge clk)
-begin
-if(reset)
-tmp<=4'b0001;
-else 
-tmp<= {tmp[2:0],tmp[3]};           //left shifitng operation
-end
-assign Q=tmp;
+`timescale 1ns/1ps 
+module shift_register (clk, reset, out);
+  
+  input clk, reset;
+  output [3:0] out;
+  
+  reg [3:0] shift;
+  
+    always @(posedge clk) begin //using non-blocking assignment 
+    if(reset) 
+      shift<=4'b0011; //initial value
+    else
+      shift<={shift[2:0], 1'b0}; //left-shifting
+    
+  end
+  
+  assign out=shift;
+  
 endmodule
