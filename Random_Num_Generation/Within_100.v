92a7230ab5f95074cbab365c68932506c8f21b6a
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2025-03-24 03:54:01 -0500

    Create Within_100.v

---
+++ Random_Num_Generation/Within_100.v
@@ -0,0 +1,16 @@
+//How to generate random number less than 100
+module random_number_tb();
+   
+  int rand_val; //32-bit signed values (both pos and neg values)
+  
+  //randomization using $random
+  initial begin 
+    for(int i=0; i<10; i++) begin
+    rand_val = $random % 100; //generates values from -99 to +99 
+    
+    //display random values 
+      $display("i= %0d, rand_val %0d",i, rand_val);
+    end
+  end
+  
+endmodule
